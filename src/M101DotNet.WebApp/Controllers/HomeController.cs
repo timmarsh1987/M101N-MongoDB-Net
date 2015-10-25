a731a57eb3564e77b6a74e46133bdfc56cdd0f62
Tim Marsh <t.marsh@my.westminster.ac.uk>
2015-10-25 12:36:59 +0000

    Check in of Homework 2.3

---
+++ src/M101DotNet.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using M101DotNet.WebApp.Models;
+using MongoDB.Bson;
+using System.Linq.Expressions;
+
+namespace M101DotNet.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        https://github.com/GSerjo/Mongodb-M101N/tree/master/Weeks/3/3.2/src/M101DotNet.WebApp
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
